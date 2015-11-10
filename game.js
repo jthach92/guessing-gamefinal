5a9549de0504ae626adcabd72848c304847b0c24
jthach92 <jthach92@gmail.com>
2015-11-09 18:57:46 -0800

    initial commit

---
+++ game.js
@@ -0,0 +1,26 @@
+var shows = "Looking";
+var guess = prompt("Which TV show should have never been canceled?");
+if (guess == shows){
+	alert("You win. You're defnitely right.");
+	console.log = function ('This user guessed correctly');	
+}
+else {
+	alert("Guess again, It is show based in San Francisco.");
+	console.log('This user guessed wrong');
+}
+var food = "Thai";
+var guess = prompt("What's my favorite type of food?");
+if (guess == food) {
+	alert("You win. Thai food is delicous all the time");
+}
+else {
+	alert("Guess again. Think SE Asia");
+}
+var color = "Blue";
+var guess = prompt("What's my favorite color?");
+if (guess == color) {
+	alert("You win. Blue is the best, purple is a close second.");
+}
+else {
+	alert("Guess again, think coooool.");
+}
